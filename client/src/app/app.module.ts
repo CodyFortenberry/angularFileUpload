cdf9776327b222f731e3d55bc93ea42469775bb9
Cody Fortenberry <cody@onpointmanufacturing.com>
2020-12-02 23:06:09 -0600

    finished with file components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UploadModule } from './upload/upload.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { FileFolderComponent } from './file-folder/file-folder.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component'
+import { MatCardModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    FileFolderComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
-    UploadModule
+    UploadModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
